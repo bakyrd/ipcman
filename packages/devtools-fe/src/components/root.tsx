8b2f70a6ff8af11c1d0aef5f560577873796fd45
Il Harper <hi@ilharper.com>
2024-02-21 15:35:06 +0800

    feat(fe): update layout

--- packages/devtools-fe/src/components/root.tsx
+++ packages/devtools-fe/src/components/root.tsx
@@ -1,10 +1,16 @@
 import type { FC } from 'react'
 import { TimelinePage } from '../pages/timeline'
 import { TitleBar } from './titlebar'
+import { Stack } from '@fluentui/react'
+import styles from './root.module.scss'
 
 export const Root: FC = () => (
   <>
-    <TitleBar />
-    <TimelinePage />
+    <Stack className={styles.root!}>
+      <Stack>
+        <TitleBar />
+        <TimelinePage />
+      </Stack>
+    </Stack>
   </>
 )
