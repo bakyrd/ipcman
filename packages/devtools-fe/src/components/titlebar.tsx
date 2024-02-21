7ec482a098ddfffbb41cb67bffc39afdba5201d4
Il Harper <hi@ilharper.com>
2024-02-21 16:44:31 +0800

    feat(fe): update titlebar

--- packages/devtools-fe/src/components/titlebar.tsx
+++ packages/devtools-fe/src/components/titlebar.tsx
@@ -1,3 +1,25 @@
+import { Pivot, PivotItem, Stack } from '@fluentui/react'
 import type { FC } from 'react'
+import styles from './titlebar.module.scss'
 
-export const TitleBar: FC = () => <></>
+export const TitleBar: FC = () => {
+  return (
+    <Stack
+      className={styles.titleBar!}
+      horizontal
+      horizontalAlign="space-between"
+    >
+      <Stack horizontal horizontalAlign="start"></Stack>
+      <Stack horizontal horizontalAlign="center">
+        <Pivot overflowBehavior="menu" overflowAriaLabel="More Views">
+          <PivotItem
+            headerText="Timeline"
+            itemIcon="TimelineProgress"
+            itemKey="timeline"
+          />
+        </Pivot>
+      </Stack>
+      <Stack horizontal horizontalAlign="end"></Stack>
+    </Stack>
+  )
+}
