4209c1cda179d33644a9e623cf23471a0f74af85
Il Harper <hi@ilharper.com>
2024-02-21 18:44:34 +0800

    feat(fe): update layout

--- packages/devtools-fe/src/components/timeline/table/index.tsx
+++ packages/devtools-fe/src/components/timeline/table/index.tsx
@@ -7,6 +7,7 @@ import {
 import type { FC } from 'react'
 import type { IpcManItem } from '../../../services/remote'
 import { ipcManDataTypeMap } from './consts'
+import styles from './index.module.scss'
 
 const columnHelper = createColumnHelper<IpcManItem>()
 
@@ -36,37 +37,63 @@ export const TimelineTable: FC<{
   const table = useReactTable({
     columns,
     data: items,
+
     getCoreRowModel: getCoreRowModel(),
+
+    enableColumnResizing: true,
+    columnResizeMode: 'onChange',
   })
   return (
     <>
-      <table>
+      <table className={styles.table}>
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
-                </th>
-              ))}
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    style={{ position: 'relative', width: header.getSize() }}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                    {header.column.getCanResize() && (
+                      <div
+                        onMouseDown={header.getResizeHandler()}
+                        onTouchStart={header.getResizeHandler()}
+                        className={`resizer ${
+                          header.column.getIsResizing() ? 'isResizing' : ''
+                        }`}
+                      ></div>
+                    )}
+                  </th>
+                )
+              })}
             </tr>
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id} style={{ width: cell.column.getSize() }}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </>
--- packages/devtools-fe/src/pages/timeline.tsx
+++ packages/devtools-fe/src/pages/timeline.tsx
@@ -2,13 +2,14 @@ import { Stack } from '@fluentui/react'
 import type { FC } from 'react'
 import { TimelineTable } from '../components/timeline/table'
 import { useRemote } from '../services/remote'
+import styles from './timeline.module.scss'
 
 export const TimelinePage: FC = () => {
   const remote = useRemote()
 
   return (
     <Stack horizontal>
-      <Stack grow>
+      <Stack className={styles.tableContainer!} grow>
         <TimelineTable items={remote.data} />
       </Stack>
       <Stack />
