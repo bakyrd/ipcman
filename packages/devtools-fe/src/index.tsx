7fd77c577fd36d8f1568ff865a5e013410f0a2db
Il Harper <hi@ilharper.com>
2024-02-25 17:17:23 +0800

    fix(fe): disable immer auto freeze

--- packages/devtools-fe/src/index.tsx
+++ packages/devtools-fe/src/index.tsx
@@ -1,8 +1,11 @@
 import { initializeIcons } from '@fluentui/react'
+import { setAutoFreeze } from 'immer'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
 import './index.scss'
+
+setAutoFreeze(false)
 
 createRoot(document.getElementById('ipcman')!).render(
   <StrictMode>
